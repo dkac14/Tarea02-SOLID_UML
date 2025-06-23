99e9c92eced93a21f3f6b14682a217c731f635df
Joshua Mero (Lau) <ariegavi@espol.edu.ec>
2025-06-22 21:31:47 -0500

    Creación de la familia de soportes
    Se crea la clase abstracta equipoSoporteTecnico e hijos según la jerarquia

---
+++ SportsPredictor/src/EquipoControlCalidad.java
@@ -0,0 +1,6 @@
+public class EquipoControlCalidad extends EquipoServicioTecnico {
+
+    public void revisar(Reporte reporte) {
+       System.out.println("Revisando detalladamente los reportes...");
+    }
+}
