683c25cd8b1dabc52697e67dd885a34e5afae4d9
Carlos Aguilera <xxguardiangamerhd@outlook.com>
2025-06-22 22:23:53 -0500

    Se agregaron varias clases para el funcionamiento correcto del programa
    Todo el desglose de Notificaciones y el enum de categoria

---
+++ SportsPredictor/src/NotificadorCorreo.java
@@ -0,0 +1,7 @@
+public class NotificadorCorreo implements Notificador{
+
+    public void notificar(String mensaje, Usuario usuario){
+        System.out.println("Enviando correo de notificación...");
+    }
+
+}
